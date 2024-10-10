4aac3564202eb768d3632a94fd8b4aedf291840b
AdrianeRuggiero <driketes@gmail.com>
2024-10-10 14:41:24 +0200

    ajoute du style sur la HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from './components/Layout'; // Import du layout
 import CustomButton from './components/CustomButton'; // Import du bouton personnalisé
+import './styles/HomePage.css';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -14,6 +15,8 @@ const HomePage = () => {
   return (
     <Layout> {/* Utilisation du Layout */}
       <div className="text-center" style={{ padding: '100px' }}>
+        <h1 class="responsive-text">Améliorez votre réeducation avec SideSync.</h1>
+        <h5 class="responsive-text"> Un outil simple, gratuit et sécurisé pour progresser efficacement.</h5>
         <div style={{ marginTop: '50px' }}>
           <CustomButton text="Démarrer le test" onClick={startTest} />
         </div>
